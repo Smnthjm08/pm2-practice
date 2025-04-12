6e9d6df38f8d9c4a0ed2fac1730988d8fd7a54bb
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-04-12 16:03:45 +0530

    new nextjs app init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen">
+      <div className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600">
+        This is a Next.js testing app for EC2
+      </div>
+      <div className="hover:underline font-mono text-orange-200">@smnthjm08</div>
+    </main>
+  );
+}
